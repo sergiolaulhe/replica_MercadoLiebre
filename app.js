538cf3dbad47b7dbb9ca9547452e3af53dd9912c
sergiolaulhe <sergiolaulhe@hotmail.com>
2022-06-24 18:46:23 -0300

    primer microdesafio de MercadoLiebre

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+const publicFolderPath = path.resolve(__dirname, "./public");
+
+app.use(express.static(publicFolderPath));
+
+
+app.listen(3000, () => {
+    console.log("Servidor en linea en puerto 3000")}
+);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/index.html"));
+})
+
